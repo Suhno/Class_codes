e6a7cd81b897e4960b2dfd79c915c25c899c6538
Suhno <131544360+Suhno@users.noreply.github.com>
2023-05-11 21:16:11 -0500

    Names.c

---
+++ Names.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+IntMain () {
+  char name [20], name2 [20], name3 [20], name4 [20], name5 [20];
+  printf ("Enter the names: );
+  scanf ("%s", name);
+  scanf ("%s", name2);
+  scanf ("%s", name3);
+  scanf ("%s", name4);
+  scanf ("%s", name5);
+  printf ("%s", name);
+  printf ("%s", name2);
+  printf ("%s", name3);
+  printf ("%s", name4);
+  printf ("%s", name5);
+  return 0;
+}
