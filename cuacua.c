cd2ab51bd04cbdc448dd9a6d832d40ae210f1fc7
Suhno <131544360+Suhno@users.noreply.github.com>
2023-08-23 21:59:41 -0500

    Add files via upload

---
+++ cuacua.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+#include <math.h> 
+int main()  
+{  
+   int s, area_square;   
+   printf("Enter the value of the sides: ");
+   scanf("%d", &s);
+	area_square=s*s;    
+   printf("Area of the square=%d",area_square);  
+    return 0;
+}
+
