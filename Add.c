e1d9e3db41847b359a28e1e4f57798c28211e523
Suhno <131544360+Suhno@users.noreply.github.com>
2023-08-23 22:32:12 -0500

    Create Add.c

---
+++ Add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+int main (){
+  int n1, n2, n3, n4, suma;
+printf ("Number? ");
+scanf ("%d", &n1);
+printf ("Number? ");
+scanf ("%d", &n2);
+printf ("Number? ");
+scanf ("%d", &n3);
+printf ("Number? ");
+scanf ("%d", &n4);
+suma=n1+n2+n3+n4;
+printf ("The answer is: %d", suma);
+return 0
+  }
