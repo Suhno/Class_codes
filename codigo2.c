1d7b38f95d6ec6f77dc5805ccd06a8f12448ab12
Suhno <131544360+Suhno@users.noreply.github.com>
2023-10-17 21:39:15 -0500

    Create codigo2.c

---
+++ codigo2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+ char pass, user;
+ printf("Ingrese su Usuario: ");
+ scanf("%c", &user);
+ printf("Ingrese su Contraseña: ");
+ scanf("%c", &pass);
+ if (user == pepito) & (pass == pepito1234) {
+ printf("Bienvenido Pepito.\n");
+ } else {
+ printf("Error.\n");
+ }
+ return 0;
+}
