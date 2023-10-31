066962b1bd2cb568f706afebe9ecd60d94873e4d
Suhno <131544360+Suhno@users.noreply.github.com>
2023-10-31 17:52:13 -0500

    Anidados.c

---
+++ Anidados.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+char selc;
+   printf("Quieres haccer una suma y/n")
+   scanf("%s", &selc)
+     if (selc == y) {
+    int num1, num2;
+    float ans;
+    printf("Ingrese el primer numero: ");
+    scanf("%s", &num1);
+    printf("Ingrese el segundo numero: ");
+    scanf("%s", &num2);
+    ans= num1 + num2
+    printf("el resultado es %f", &ans);
+   } else {
+     printf("Error.\n");
+   }
+return 0;
+}
